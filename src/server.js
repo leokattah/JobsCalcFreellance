c96622a81b8032de824f0820d23edad7c0678f60
leokattah <leokattah@gmail.com>
2021-03-31 20:22:19 -0300

    day 2 finished

--- src/server.js
+++ src/server.js
@@ -1,20 +1,17 @@
-//Importando o pacote express e colocando em uma variável que chamei de express;
-const express = require("express");
-const routes = require("./routes");
-//Colocando o a função express() na variável que chamei de server
+const express = require("express")
 const server = express()
+const routes = require("./routes")
 
-//Usando template engine
-server.set("view engine" , "ejs");
+// usando template engine
+server.set('view engine',  'ejs')
 
-//habilitar arquivos estáticos. O comando abaixo sugere a criação de todas as rotas que estão na pasta public de forma automática. 
-server.use(express.static("public"));
+//habilitar arquivos statics
+server.use(express.static("public"))
 
-//routes
-server.use(routes);
-/*
-O objeto server usou uma propriedade listens para levantar o servidor.
-primeiro argumento: 3000 (número da porta escolhida)
-segundo argumento: função que será retornada. 
-*/
-server.listen(3000, () => console.log("Servidor rodando"));
+// usar o req.body
+server.use(express.urlencoded({ extended: true }))
+
+// routes
+server.use(routes)
+
+server.listen(3000, () => console.log('rodando'))
